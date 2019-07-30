e0930afbb3c4692925ff33e1796d45fd320da63b
lxt <lxt112566@163.com>
2019-07-31 00:56:55 +0800

    增加了购物车的全选功能

--- demo/src/pages/IndexPage/shopping/shoppingIndex.js
+++ demo/src/pages/IndexPage/shopping/shoppingIndex.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import "./shoppingIndex.scss";
 import { inject, observer } from "mobx-react";
-import ShopEdit from "../../../component/shopEdit"; //编辑
-import ShopFinish from "../../../component/shopFinish"; //编辑
+
 
 @inject("card")
 @observer
@@ -10,67 +9,79 @@ class ShoppingIndex extends Component {
   constructor() {
     super();
     this.state = {
-      isEdit: true, //默认的是完成页面
-      isChecked:true //判断有没有选中
+      isFinish: true, //默认的是完成页面
+      isChecked: true //判断有没有选中
     };
   }
 
   componentDidMount() {
+    //获取购物车里面的数据
     this.props.card.getCartDataModule();
-
+    //获取页面全选的数据
+    this.props.card.changeInitFinishCheckedFn();
   }
 
   //切换页面
   changeEdit = e => {
     this.setState({
-      isEdit: !this.state.isEdit
+      isFinish: !this.state.isFinish
     });
   };
 
-  //切换全选和反选
-  changeChecked=()=>{
-    console.log(123);
-    this.setState({
-      isChecked:!this.state.isChecked
-    },()=>{
-      let data=this.props.card.getCartData;
-      console.log(data)
-      console.log(data.cartList);
-      let arr=data.cartList.map((item,index)=>{
-        return item.product_id
-      })
-      console.log(arr);
-      let productIdsData= arr.join();
-      console.log(productIdsData);
-
-
-      if(this.state.isChecked){
-        this.props.card.postCartCheckModule({
-          isChecked:1,
-           productIds:productIdsData,
-
-        })
-
-      }else{
-        this.props.card.postCartCheckModule({
-          isChecked:0,
-           productIds:productIdsData,
-
-        })
-      }
-
-
+//切换完成页面的选中的按钮
+  changeChecked(data){
+    // isChecked: 1,
+    // productIds: productIdsData
+    console.log(data);
+    let checked=data.checked===0?1:0;
+    this.props.card.postCartCheckModule({
+         isChecked: checked,
+     productIds: data.product_id
     })
-
     
   }
 
+  // //切换全选和反选
+  // changeChecked = () => {
+  //   console.log(123);
+  //   this.setState(
+  //     {
+  //       isChecked: !this.state.isChecked
+  //     },
+  //     () => {
+  //       let data = this.props.card.getCartData;
+  //       console.log(data);
+  //       console.log(data.cartList);
+  //       let arr = data.cartList.map((item, index) => {
+  //         return item.product_id;
+  //       });
+  //       console.log(arr);
+  //       let productIdsData = arr.join();
+  //       console.log(productIdsData);
+
+  //       if (this.state.isChecked) {
+  //         this.props.card.postCartCheckModule({
+  //           isChecked: 1,
+  //           productIds: productIdsData
+  //         });
+  //       } else {
+  //         this.props.card.postCartCheckModule({
+  //           isChecked: 0,
+  //           productIds: productIdsData
+  //         });
+  //       }
+  //     }
+  //   );
+  // };
+
   render() {
-    let data=this.props.card.getCartData;
+    let data = this.props.card.getCartData;
     console.log(data);
-    
-    
-    let {isEdit}=this.state;
+    //postCartCheckData
+    let {postCartCheckData}=this.props.card;
+    console.log(postCartCheckData);
+
+    let { isFinish } = this.state;
     return (
       <div className="shopBox">
         {/* 头部 */}
@@ -90,40 +101,108 @@ class ShoppingIndex extends Component {
         </ul>
         {/* 内容 */}
         <div className="main">
+          {data.cartList &&
+            data.cartList.map((item, index) => {
+              return (
+              
+                  <div className="shopFinshBox" key={item.goods_name}>
+                   {
+                     this.state.isFinish?<div className="check" onClick={()=>{this.changeChecked(item)}}>
+                     <img
+                       src={
+                         item.checked === 1
+                           ? "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACYAAAAmCAMAAACf4xmcAAAAQlBMVEUAAACrKyurKyurKyurKyurKyurKyurKyurKyurKyurKyurKyurKyurKyurKyvw19exOzv////z4uK1Q0Pt0dGxOjp+CNkCAAAADnRSTlMARVn7B9cVoc/jIWtnJIM++AMAAADUSURBVDjLndRLEoMgEEVRPyCg+FAh+99qYqmAabFL7/hMaKCrN/VWyRZopbJ9ETUaWbq5RLXBX6YmSChcpMRZdRKX6e6kDAqZzAmNYlpEpnCTimfEbfWmhLlnZp8qmLY5a47pVY0oNIWArfV+h5Jy88FsNg2q3JTNRLIK8sd4hTZnwfmzSuVsmRdPFGV+d1S18QjJUQUZB5IcVVBxvMlRBRsvKzmq0JOr9y58yNU/eEj8s3zyyPkvcyQk9wH57/xwOfCrhl9cNMGswdQ4HEt1GKsXfQHGSThPkNi75AAAAABJRU5ErkJggg=="
+                           : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACYAAAAmCAMAAACf4xmcAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAABCUExURUdwTMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzAV+Z0EAAAAVdFJOUwAJ+KUEFTPay2bzRXdZ7RkhmJ6qJOWhY+QAAAEDSURBVDjLnZTplsIgDIUNWwK2tdt9/1cdxHGmVcAc+dH25Hw0+71cvjhztDIZM4mNc4txo+BwZKxSVwbSFoMn8iFuCeDrG0RLNkc6GGK+ttCZ8gIzuJcgBgPxJ4rB4T2OkM0HjgRyq8V7Y8i/3/V06YVb/nKECa0qBYPffB1jaFd8AD8+RrBrY8R41FkQew2MkPtrR6IeRglzoW1/HrbizfZ9Pv8jCH0slOAm+D7mMeUn4PoYwegxpVNlCsqCKMurbJay9R8GyT0HSTmWeciTYsh7K+MPK1MW0H9eQOU652sqcch+15rUrFQXLpuFy7ksXLYuXDUZbBZ9v4sqiqju34jyD97JD4dkfgo1AAAAAElFTkSuQmCC"
+                       }
+                       alt=""
+                     />
+                   </div>:<div className="check" onClick={()=>{this.props.changeEditItemChecked(item)}}>
+                     <img
+                       src={
+                         item.flag
+                           ? "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACYAAAAmCAMAAACf4xmcAAAAQlBMVEUAAACrKyurKyurKyurKyurKyurKyurKyurKyurKyurKyurKyurKyurKyurKyvw19exOzv////z4uK1Q0Pt0dGxOjp+CNkCAAAADnRSTlMARVn7B9cVoc/jIWtnJIM++AMAAADUSURBVDjLndRLEoMgEEVRPyCg+FAh+99qYqmAabFL7/hMaKCrN/VWyRZopbJ9ETUaWbq5RLXBX6YmSChcpMRZdRKX6e6kDAqZzAmNYlpEpnCTimfEbfWmhLlnZp8qmLY5a47pVY0oNIWArfV+h5Jy88FsNg2q3JTNRLIK8sd4hTZnwfmzSuVsmRdPFGV+d1S18QjJUQUZB5IcVVBxvMlRBRsvKzmq0JOr9y58yNU/eEj8s3zyyPkvcyQk9wH57/xwOfCrhl9cNMGswdQ4HEt1GKsXfQHGSThPkNi75AAAAABJRU5ErkJggg=="
+                           : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACYAAAAmCAMAAACf4xmcAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAABCUExURUdwTMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzAV+Z0EAAAAVdFJOUwAJ+KUEFTPay2bzRXdZ7RkhmJ6qJOWhY+QAAAEDSURBVDjLnZTplsIgDIUNWwK2tdt9/1cdxHGmVcAc+dH25Hw0+71cvjhztDIZM4mNc4txo+BwZKxSVwbSFoMn8iFuCeDrG0RLNkc6GGK+ttCZ8gIzuJcgBgPxJ4rB4T2OkM0HjgRyq8V7Y8i/3/V06YVb/nKECa0qBYPffB1jaFd8AD8+RrBrY8R41FkQew2MkPtrR6IeRglzoW1/HrbizfZ9Pv8jCH0slOAm+D7mMeUn4PoYwegxpVNlCsqCKMurbJay9R8GyT0HSTmWeciTYsh7K+MPK1MW0H9eQOU652sqcch+15rUrFQXLpuFy7ksXLYuXDUZbBZ9v4sqiqju34jyD97JD4dkfgo1AAAAAElFTkSuQmCC"
+                       }
+                       alt=""
+                     />
+                   </div>
 
-          {data.cartList&&data.cartList.map((item,index)=>{
-            return (
-              isEdit?<ShopFinish key={"finsh"+index} data={item} {...this.props}/> : <ShopEdit key={"edit"+index} data={item} {...this.props}/>
+                   } 
+                    <div className="img">
+                      <img src={item.list_pic_url} alt="" />
+                    </div>
 
-            )
-          })}
-        
+                    {this.state.isFinish ? (
+                      <div className="shopFinshBox_main">
+                        <div className="shopFinshMain_top">
+                          <h3>{item.goods_name}</h3>
+                          <div className="num">
+                            x<span>{item.number}</span>
+                          </div>
+                        </div>
+                        <div className="price">￥{item.market_price}</div>
+                      </div>
+                    ) : (
+                      <div className="itemBox_main">
+                       
+                        <div className="title">{item.goods_name}</div>
+                        <div className="numPrice">
+                          <div className="price">￥{item.market_price}</div>
+
+                          <div className="numBox">
+                            <span>-</span>
+                            <span className="num">{item.number}</span>
+                            <span>+</span>
+                          </div>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                
+              );
+            })}
         </div>
         {/* 选项 */}
         <div className="shop">
           <div className="checkedBox">
             <div className="choice">
-              <div className="check" onClick={()=>this.changeChecked()}>
+              {this.state.isFinish? <div
+                className="check"
+                onClick={() => this.props.card.changeChecked()}
+              >
                 <img
                   src={
-                    this.state.isChecked
+                    this.props.card.FinishIsChecked
                       ? "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACYAAAAmCAMAAACf4xmcAAAAQlBMVEUAAACrKyurKyurKyurKyurKyurKyurKyurKyurKyurKyurKyurKyurKyurKyvw19exOzv////z4uK1Q0Pt0dGxOjp+CNkCAAAADnRSTlMARVn7B9cVoc/jIWtnJIM++AMAAADUSURBVDjLndRLEoMgEEVRPyCg+FAh+99qYqmAabFL7/hMaKCrN/VWyRZopbJ9ETUaWbq5RLXBX6YmSChcpMRZdRKX6e6kDAqZzAmNYlpEpnCTimfEbfWmhLlnZp8qmLY5a47pVY0oNIWArfV+h5Jy88FsNg2q3JTNRLIK8sd4hTZnwfmzSuVsmRdPFGV+d1S18QjJUQUZB5IcVVBxvMlRBRsvKzmq0JOr9y58yNU/eEj8s3zyyPkvcyQk9wH57/xwOfCrhl9cNMGswdQ4HEt1GKsXfQHGSThPkNi75AAAAABJRU5ErkJggg=="
                       : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACYAAAAmCAMAAACf4xmcAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAABCUExURUdwTMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzAV+Z0EAAAAVdFJOUwAJ+KUEFTPay2bzRXdZ7RkhmJ6qJOWhY+QAAAEDSURBVDjLnZTplsIgDIUNWwK2tdt9/1cdxHGmVcAc+dH25Hw0+71cvjhztDIZM4mNc4txo+BwZKxSVwbSFoMn8iFuCeDrG0RLNkc6GGK+ttCZ8gIzuJcgBgPxJ4rB4T2OkM0HjgRyq8V7Y8i/3/V06YVb/nKECa0qBYPffB1jaFd8AD8+RrBrY8R41FkQew2MkPtrR6IeRglzoW1/HrbizfZ9Pv8jCH0slOAm+D7mMeUn4PoYwegxpVNlCsqCKMurbJay9R8GyT0HSTmWeciTYsh7K+MPK1MW0H9eQOU652sqcch+15rUrFQXLpuFy7ksXLYuXDUZbBZ9v4sqiqju34jyD97JD4dkfgo1AAAAAElFTkSuQmCC"
                   }
                   alt=""
                 />
-              </div>
-              <div>已选(3)</div>
-              {this.state.isEdit ? <div>￥0</div> : null}
+              </div>: <div
+                className="check"
+                onClick={() => this.props.card.changeEditChecked()}
+              >
+                <img
+                  src={
+                    this.props.card.EditIsChecked
+                      ? "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACYAAAAmCAMAAACf4xmcAAAAQlBMVEUAAACrKyurKyurKyurKyurKyurKyurKyurKyurKyurKyurKyurKyurKyurKyvw19exOzv////z4uK1Q0Pt0dGxOjp+CNkCAAAADnRSTlMARVn7B9cVoc/jIWtnJIM++AMAAADUSURBVDjLndRLEoMgEEVRPyCg+FAh+99qYqmAabFL7/hMaKCrN/VWyRZopbJ9ETUaWbq5RLXBX6YmSChcpMRZdRKX6e6kDAqZzAmNYlpEpnCTimfEbfWmhLlnZp8qmLY5a47pVY0oNIWArfV+h5Jy88FsNg2q3JTNRLIK8sd4hTZnwfmzSuVsmRdPFGV+d1S18QjJUQUZB5IcVVBxvMlRBRsvKzmq0JOr9y58yNU/eEj8s3zyyPkvcyQk9wH57/xwOfCrhl9cNMGswdQ4HEt1GKsXfQHGSThPkNi75AAAAABJRU5ErkJggg=="
+                      : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACYAAAAmCAMAAACf4xmcAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAABCUExURUdwTMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzAV+Z0EAAAAVdFJOUwAJ+KUEFTPay2bzRXdZ7RkhmJ6qJOWhY+QAAAEDSURBVDjLnZTplsIgDIUNWwK2tdt9/1cdxHGmVcAc+dH25Hw0+71cvjhztDIZM4mNc4txo+BwZKxSVwbSFoMn8iFuCeDrG0RLNkc6GGK+ttCZ8gIzuJcgBgPxJ4rB4T2OkM0HjgRyq8V7Y8i/3/V06YVb/nKECa0qBYPffB1jaFd8AD8+RrBrY8R41FkQew2MkPtrR6IeRglzoW1/HrbizfZ9Pv8jCH0slOAm+D7mMeUn4PoYwegxpVNlCsqCKMurbJay9R8GyT0HSTmWeciTYsh7K+MPK1MW0H9eQOU652sqcch+15rUrFQXLpuFy7ksXLYuXDUZbBZ9v4sqiqju34jyD97JD4dkfgo1AAAAAElFTkSuQmCC"
+                  }
+                  alt=""
+                />
+              </div>}
+             
+              {this.state.isFinish ?<div>已选{data.cartTotal&&data.cartTotal.checkedGoodsCount}商品</div>:<div>已选择{this.props.card.EditCheckedCount}</div>}
+              {this.state.isFinish ? <div>￥{data.cartTotal&&data.cartTotal.checkedGoodsAmount}</div> : null}
             </div>
           </div>
 
           <div className="edit" onClick={e => this.changeEdit(e)}>
-            {this.state.isEdit ? "编辑" : "完成"}
+            {this.state.isFinish ? "编辑" : "完成"}
           </div>
 
           <div className="option">
-            {this.state.isEdit ? "下单" : "删除所选"}
+            {this.state.isFinish ? "下单" : "删除所选"}
           </div>
         </div>
       </div>
--- demo/src/store/module/shoppingCart/addCart.js
+++ demo/src/store/module/shoppingCart/addCart.js
@@ -1,64 +1,155 @@
 /**
  * 购物车
  */
-import {observable,action}  from "mobx";
+import { observable, action } from "mobx";
 import {
   postAddCartServer,
   getCartDataServer,
   getCartNumServer,
   postCartCheckServer
+} from "../../../servies";
+
+export default class cardShop {
+  @observable state = {};//添加购物车返回的数据的接口
+  @observable getCartData = [];//购物车的数据
+  @observable getCartNumData = 0;//获取到购物车商品数量
+  @observable postCartCheckData = "";//商品选中方法获取的数据
+  @observable FinishIsChecked = ""; //完成页面默认的全选
+  @observable EditIsChecked = ""; //编辑页面默认的全选
+  @observable EditCheckedCount= 0;//编辑页面选中的数量
+
+  //给页面全选的按钮赋值
+
+  @action async changeInitFinishCheckedFn() {
+    let data = await this.getCartDataModule();
+    //获取到完成页面的全选的状态
+    let Finishflag =
+      this.getCartData &&
+      this.getCartData.cartList.every((item, index) => {
+        return item.checked;
+      });
+//获取到编辑页面的全选的状态
+      let Editflag =
+      this.getCartData &&
+      this.getCartData.cartList.every((item, index) => {
+        return item.flag;
+      });
+    this.EditIsChecked = Editflag;
+    this.FinishIsChecked=Finishflag;
+  }
+
+  //修饰方法
+  //添加到购物;
+  @action postAddCartModule(data) {
+    postAddCartServer(data).then(res => {
+      console.log(res);
+      this.state = res.data.data;
+      this.getCartNumModule();
+    });
+  }
+
+  //获取到购物车的数据
+
+  @action async getCartDataModule() {
+    let data = await getCartDataServer();
+    console.log(data);
+    let newData=data.data.cartList.map((item,index)=>{
+      item.flag=false;
+      return item;
+    })
+    console.log(newData);
+    let obj={cartList:newData,cartTotal:data.data.cartTotal}
+
+    this.getCartData =obj;
+  }
+
+  //获取到购物车商品数量
+  @action async getCartNumModule() {
+    console.log(123);
+    let data = await getCartNumServer();
+    console.log(data);
+    this.getCartNumData = data.data.cartTotal;
+  }
+
+  //数据的全选和反选
+  @action async postCartCheckModule(params) {
+    console.log(params);
+    let data = await postCartCheckServer(params);
+    console.log(data);
+    this.postCartCheckData = data.data;
+    this.getCartDataModule(); //重新获取的数购物车的数据
+    this.changeInitFinishCheckedFn();
+  }
+
+  //切换完成页面的全选和反选
+  @action changeChecked() {
+    console.log(123);
+    this.FinishIsChecked = !this.FinishIsChecked;
+    let arr = this.getCartData.cartList.map((item, index) => {
+      return item.product_id;
+    });
+    console.log(arr);
+    let productIdsData = arr.join();
+    console.log(productIdsData);
+
+    if (this.FinishIsChecked) {
+      this.postCartCheckModule({
+        isChecked: 1,
+        productIds: productIdsData
+      });
+
+    } else {
+      this.postCartCheckModule({
+        isChecked: 0,
+        productIds: productIdsData
+      });
+    }
+  }
+
+  //切换编辑页面的全选和反选
+  @action changeEditChecked() {
+    console.log(123);
+    this.EditIsChecked = !this.EditIsChecked;
+   let data= this.getCartData.cartList.map((item,index)=>{
+      item.flag=this.EditIsChecked;
+      return item;
+    })
+    if(this.EditIsChecked){
+      this.EditCheckedCount=this.getCartData.cartList.length;
+    }else{
+      this.EditCheckedCount=0;
+    }
+
+    this.getCartData.cartList=data;
+
+  }
+
+   //切换编辑页面每一项的全选和反选功能
+   @action changeEditItemChecked(data) {
+     console.log(data);
+
+     let newData=this.getCartData.cartList.map((item,index)=>{
+        if(item.product_id=== data.product_id){
+          item.flag=!item.flag;
+        }
+        return item;
+     })
+     //编辑页面的全选按钮的状态
+     this.EditIsChecked= newData.every((item,index)=>{
+       return item.flag;
+     })
+     //编辑页面数据的选中的状态
+     this.getCartData.cartList=newData;
+
+     //编辑页面的数量
+    let newDataCount= newData.filter((item,index)=>{
+       return item.flag;
+     })
+
+     this.EditCheckedCount=newDataCount.length;
+
+
+
+  }
   
-  } from "../../../servies"
-
-export default class cardShop{  
-    @observable state={};
-    @observable cardNum=0;
-    @observable getCartData=[];
-    @observable getCartNumData=0;
-    @observable postCartCheckData="";
-    @observable isChecked="";//默认的选中状态
-
-    //修饰方法
-    //添加到购物;
-    @action postAddCartModule(data){
-      postAddCartServer (data).then(res=>{
-        console.log(res);
-          this.state = res.data.data;
-          this.getCartNumModule();
-
-       } 
-      )
-    }
-    
-    //获取到购物车的数据
-
-    @action async getCartDataModule(){
-      let data=await getCartDataServer();
-      console.log(data);
-      this.getCartData=data.data;
-      
-    }
-
-    //获取到购物车商品数量
-    @action async getCartNumModule(){
-      console.log(123);
-      let data=await getCartNumServer();
-      console.log(data);
-      this.getCartNumData=data.data.cartTotal;
-      
-    }
-
-    //数据的全选和反选
-    @action async postCartCheckModule(params){
-      console.log(123);
-      let data=await postCartCheckServer(params);
-      console.log(data);
-      this.postCartCheckData=data;
-      this.getCartDataModule();
-      
-    }
-
-
-
-
 }

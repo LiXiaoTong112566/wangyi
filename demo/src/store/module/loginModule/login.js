0000bae19eb5b5af42a431401ecae8f4fa209ef3
lxt <lxt112566@163.com>
2019-08-02 00:08:22 +0800

    修改好的页面

--- demo/src/store/module/loginModule/login.js
+++ demo/src/store/module/loginModule/login.js
@@ -1,7 +1,7 @@
 import { postLogin } from "../../../servies/index";
 import { setCookie } from "../../../utils/index";
 import { observable, action } from "mobx";
-
+import { Toast } from "antd-mobile";
 export default class IndexPageModule {
   @observable dataFlag = -1; //登录返回来的数据
   @observable userName = "";
@@ -9,15 +9,16 @@ export default class IndexPageModule {
   //修饰方法
   @action getLogin(data) {
     postLogin(data).then(res => {
-       this.dataFlag=res.data;
-      console.log(res)
+      this.dataFlag = res.data;
+      console.log(res);
       if (res.errno === 0) {
-        alert("登录成功");
+        Toast.success("登录成功", 1);
+
         this.dataFlag = res.errno;
         this.userName = res.data.mobile;
         setCookie(res.data.sessionKey);
       } else {
-        alert("登录失败");
+        Toast.offline("用户名或密码有误", 1);
         this.dataFlag = res.errno;
       }
     });
